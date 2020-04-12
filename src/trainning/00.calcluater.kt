35c743228eead62ab6641180dee5363f80b789b2
admin <k.kubo.private.mobile@gmail.com>
2020-04-12 13:14:56 +0900

    all

---
+++ src/trainning/00.calcluater.kt
@@ -0,0 +1,19 @@
+package trainning/* 電卓計算機の実装 */
+
+/**
+ * 前置きを後起きに変換する記法:  F(式, operator, 式) == F(式)F(式)operater
+ * 文字を読み込む => 数字
+ * 文字を読み込む => 演算子
+ * 文字を読み込む => 数字
+ * 文字を読み込む => 優先度の低い演算子を発見
+ * F(数字 + 数字, +, (残り)
+ * F(数字) = 数字
+ * */
+fun main() {
+    val expressionRegex = Regex("[1-9][0-9]*|0")
+    val spaceRegex = Regex("¥s")
+    val operatorRegex = Regex("x|¥-|¥+|/")
+    val n = expressionRegex.matches("0")
+    println(spaceRegex.matches(" "))
+    /* ファイルの操作の理解が必要 */
+}
