6b11bc44e1b68451a0a909776f655cb1190cd07d
admin <k.kubo.private.mobile@gmail.com>
2020-04-13 01:27:05 +0900

    created calculator

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ src配下でないと効かない。
 ## Kotlin to learn
 - [x] ファイルの操作の理解が必
 - [x] Stdlibの理解が必要
+- [ ] Regexp => Parseのため
+- [ ] when => 効果的な分岐のため
+- [ ] Testコードの設定方法
 - [ ] packageの管理の理解
 - [ ] IDEAのコード補完の設定
 - [ ] IDEAのデバグ設定   
--- src/trainning/00.calcluater.kt
+++ src/trainning/00.calcluater.kt
@@ -1,6 +1,8 @@
 package trainning
 
 import java.io.File
+import java.io.InputStreamReader
+import java.lang.Exception
 
 /* 電卓計算機の実装 */
 
@@ -14,20 +16,174 @@ import java.io.File
  * F(数字) = 数字
  * */
 fun main() {
-    val expressionRegex = Regex("[1-9][0-9]*|0")
-    val spaceRegex = Regex("¥s")
-    val operatorRegex = Regex("x|¥-|¥+|/")
-    val n = expressionRegex.matches("0")
+    val numberRegex = Regex("[1-9][0-9]*|0")
+    val spaceRegex = Regex("\\s")
+    val operatorRegex = Regex("\\*|-|\\+|/")
+    val n = numberRegex.matches("0")
     println(spaceRegex.matches(" "))
 
     val path = System.getProperty("user.dir")
-    val reader = File(path).resolve("files/00.txt").reader()
-    fun doSomething(f: (char: Char) -> Unit) {
+    val reader: InputStreamReader = File(path).resolve("files/00.txt").reader()
+
+
+    fun tokenize(reader: InputStreamReader): MutableList<Token> {
         var byte = reader.read()
+        var state: String = "empty"
+        val tokens = mutableListOf<Token>()
+        var tokenStr: String = "empty"
         while (byte != -1) {
-            val char = byte.toChar()
-            f(char)
+            val char: String = byte.toChar().toString()
+
+            when {
+                numberRegex.matches(char) -> {
+                    when (state) {
+                        "empty" -> {
+                            state = "num"
+                            tokenStr = char
+                        }
+                        "operator" -> {
+                            state = "num"
+                            tokens.add(OperatorToken(tokenStr))
+                            tokenStr = char
+                        }
+                        "num" -> {
+                            state = "num"
+                            tokenStr += char
+                        }
+                    }
+                }
+                operatorRegex.matches(char) -> {
+                    when (state) {
+                        "empty" -> {
+                            state = "operator"
+                            tokenStr = char
+                        }
+                        "operator" -> {
+                            throw Exception("operator should be successive")
+                        }
+                        "num" -> {
+                            state = "operator"
+                            tokens.add(NumberToken(tokenStr))
+                            tokenStr = char
+                        }
+                    }
+
+                }
+                spaceRegex.matches(char) -> {
+                    when (state) {
+                        "empty" -> {
+                            state = "empty"
+                        }
+                        "num" -> {
+                            state = "empty"
+                            tokens.add(NumberToken(tokenStr))
+                            tokenStr = ""
+                        }
+                        "operator" -> {
+                            state = "empty"
+                            tokens.add(OperatorToken(tokenStr))
+                            tokenStr = ""
+                        }
+                    }
+                }
+            }
+
             byte = reader.read()
         }
+        if (state === "num") {
+            tokens.add(NumberToken(tokenStr))
+        }
+
+        return tokens
+    }
+
+    val opStack: MutableList<Token> = mutableListOf()
+    val reversedTokens: MutableList<Token> = mutableListOf()
+    opStack.add(OperatorToken("start"))
+    fun popStack(opStack: MutableList<Token>, opToken: Token, reversedTokens: MutableList<Token>) {
+        val top = opStack.last()
+        if (opToken.toString() == "*" || opToken.toString() == "/") {
+            if (top.toString() == "*" || top.toString() == "/") {
+                reversedTokens.add(opStack.removeAt(opStack.size - 1))
+                popStack(opStack, opToken, reversedTokens)
+            }
+        }
+        if (opToken.toString() == "+" || opToken.toString() == "-") {
+            if (top.toString() == "*" || top.toString() == "/" || top.toString() == "+" || top.toString() == "-") {
+                reversedTokens.add(opStack.removeAt(opStack.size - 1))
+                popStack(opStack, opToken, reversedTokens)
+            }
+        }
+        if (opToken.toString() == "end") {
+            if (top.toString() != "start") {
+                reversedTokens.add(opStack.removeAt(opStack.size - 1))
+                popStack(opStack, opToken, reversedTokens)
+            }
+        }
+    }
+
+    val tokens = tokenize(reader)
+    tokens.forEachIndexed() { i: Int, token: Token ->
+        when (token.tokenType) {
+            "num" -> {
+                reversedTokens.add(token)
+            }
+            "op" -> {
+                popStack(opStack, token, reversedTokens)
+                opStack.add(token)
+            }
+        }
+        if (i == tokens.size - 1) {
+            popStack(opStack, OperatorToken("end"), reversedTokens)
+        }
+    }
+    reversedTokens.forEach {
+        println("${it.tokenType}: $it")
+    }
+    val numStack = mutableListOf<Int>()
+    reversedTokens.forEach {
+        if (it.tokenType == "num") {
+            numStack.add(it.toString().toInt())
+        }
+        if (it.tokenType == "op") {
+            val b = numStack.removeAt(numStack.size - 1)
+            val a = numStack.removeAt(numStack.size - 1)
+            var answer: Int = 0
+            when (it.toString()) {
+                "*" -> {
+                    answer = a * b
+                }
+                "/" -> {
+                    answer = a / b
+                }
+                "+" -> {
+                    answer = a + b
+                }
+                "-" -> {
+                    answer = a - b
+                }
+            }
+            numStack.add(answer)
+        }
+    }
+    println(numStack[0])
+}
+
+interface Token {
+    val tokenType: String
+    override fun toString(): String
+}
+
+class NumberToken(private val num: String) : Token {
+    override val tokenType = "num"
+    override fun toString(): String {
+        return num
+    }
+}
+
+class OperatorToken(private val operator: String) : Token {
+    override val tokenType = "op"
+    override fun toString(): String {
+        return operator
     }
 }
