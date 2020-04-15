91284f6f445cf35e341b3e59ba8f7632fddc1a11
admin <k.kubo.private.mobile@gmail.com>
2020-04-16 02:43:10 +0900

    add comment

--- src/varTable/00.varTable.kt
+++ src/varTable/00.varTable.kt
@@ -11,6 +11,7 @@ fun main() {
     /* 変数表は順番を持つ */
     val table: MutableList<VarTableRow> = mutableListOf()
     /* p17. テストケースを利用してシミュレートしたい */
+    /* 変数名に対してIndexしたいので、tableはクラスでラップしたい。 */
     table.add(object : VarTableRow {
         override var name = "abc"
         override var type = "real"
